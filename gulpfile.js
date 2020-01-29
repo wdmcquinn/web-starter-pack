1612e4cf7b88004bbf8e88313c63cd44aab0bb33
Doug McQuinn <wdmcquinn@gmail.com>
2020-01-29 16:51:33 -0500

    CHORE: Added style to gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -2,23 +2,11 @@ const gulp = require ('gulp');
 const browserSync = require('browser-sync').create();
 const sass = require('gulp-sass');
 
-// Compile Sass from scss folder to css folder
-gulp.task('sass', function(){
-    return gulp.src(['src/scss/*.scss'])
-      .pipe(sass())
-      .pipe(gulp.dest('src/css'))
-      .pipe(browserSync.stream());
-});
+//compile scss to css
+function style(){
+    return gulp.src('./scss/**/*.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('./css'))
+}
 
-//Watch folders and Serve files
-gulp.task('serve', gulp.series(['sass'], function(){
-    browserSync.init({
-        server: './src'
-    });
-
-    gulp.watch(['src/scss/*.scss'], gulp.series(['sass']));
-    gulp.watch(['src/*.html']).on('change', browserSync.reload);
-}));
-
-// Defautl Task
-gulp.task('default', gulp.series(['serve']));
+exports.style = style;
