73209cad69423eb71da783c01e4891f3697d9e67
Doug McQuinn <wdmcquinn@gmail.com>
2020-01-30 11:13:49 -0500

    type: refactor
    I adjusted the gulpfile to use functions for version 4 and cleaned up the formating

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,32 @@
-const gulp = require ('gulp');
-const browserSync = require('browser-sync').create();
-const sass = require('gulp-sass');
+const gulp = require ('gulp'),
+    browserSync = require('browser-sync').create(),
+    sass = require('gulp-sass');
+
+const root = './src/',
+    styleWatch = root + 'scss/**/*.scss',
+    htmlWatch = root + '**/*.html',
+    jsWatch = root + 'js/**/*.js'
+;
 
 //compile scss to css
 function style(){
-    return gulp.src('./scss/**/*.scss')
+    return gulp.src(styleWatch)
         .pipe(sass())
-        .pipe(gulp.dest('./css'))
+        .pipe(gulp.dest(root+'css/'))
+        .pipe(browserSync.stream());
 }
 
+function watch(){
+    browserSync.init({
+        server: {
+            baseDir: root
+        }
+    });
+    gulp.watch(styleWatch, style);
+    gulp.watch(htmlWatch).on('change', browserSync.reload);
+    gulp.watch(jsWatch).on('change', browserSync.reload);
+}
+
+
 exports.style = style;
+exports.watch = watch;
