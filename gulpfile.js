f57d8c33b98d1fc6fabc71baf66dfa579ecdeb90
Doug McQuinn <wdmcquinn@gmail.com>
2020-01-30 14:45:02 -0500

    feature: Added automation to build project into dist directory

--- gulpfile.js
+++ gulpfile.js
@@ -1,32 +1,48 @@
-const gulp = require ('gulp'),
+const { gulp, watch, series, src, dest } = require ('gulp'),
     browserSync = require('browser-sync').create(),
     sass = require('gulp-sass');
 
 const root = './src/',
     styleWatch = root + 'scss/**/*.scss',
     htmlWatch = root + '**/*.html',
-    jsWatch = root + 'js/**/*.js'
+    jsWatch = root + 'js/**/*.js',
+    dist = './dist'
 ;
 
 //compile scss to css
 function style(){
-    return gulp.src(styleWatch)
+    return src(styleWatch)
         .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest(root+'css/'))
+        .pipe(dest(root+'css/'))
         .pipe(browserSync.stream());
 }
 
-function watch(){
+function watcher(){
     browserSync.init({
         server: {
             baseDir: root
         }
     });
-    gulp.watch(styleWatch, style);
-    gulp.watch(htmlWatch).on('change', browserSync.reload);
-    gulp.watch(jsWatch).on('change', browserSync.reload);
+    watch(styleWatch, style);
+    watch(htmlWatch).on('change', browserSync.reload);
+    watch(jsWatch).on('change', browserSync.reload);
+}
+function copy(path, pathout){
+    return src(path)
+    .pipe(dest(pathout));
+}
+
+function build(cb){
+   copy(htmlWatch, dist);
+   copy(root+'css/**/*.css', dist+'/css/')
+   copy(root+'js/**/*.js', dist+'/js/')
+   cb();
 }
 
 
 exports.style = style;
-exports.watch = watch;
+exports.watcher = watcher;
+exports.build = build;
+exports.copy;
+
+exports.default = series(watcher)
